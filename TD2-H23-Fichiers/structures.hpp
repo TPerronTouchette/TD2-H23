1716b97968152e73d5118e2634b416b92ac05d4b
TPerronTouchette <113641343+TPerronTouchette@users.noreply.github.com>
2023-02-11 17:35:09 -0500

    et ca

--- TD2-H23-Fichiers/structures.hpp
+++ TD2-H23-Fichiers/structures.hpp
@@ -6,24 +6,24 @@
 struct Film; struct Acteur; // Permet d'utiliser les types alors qu'ils seront défini après.
 
 struct ListeFilms {
-	int capacite=0, nElements=0;
+	int capacite, nElements;
 	Film** elements; // Pointeur vers un tableau de Film*, chaque Film* pointant vers un Film.
 };
 
 struct ListeActeurs {
-	int capacite=0, nElements=0;
+	int capacite, nElements;
 	Acteur** elements; // Pointeur vers un tableau de Acteur*, chaque Acteur* pointant vers un Acteur.
 };
 
 struct Film
 {
-	std::string titre="", realisateur = ""; // Titre et nom du réalisateur (on suppose qu'il n'y a qu'un réalisateur).
-	int anneeSortie=0, recette=0; // Année de sortie et recette globale du film en millions de dollars
+	std::string titre, realisateur; // Titre et nom du réalisateur (on suppose qu'il n'y a qu'un réalisateur).
+	int anneeSortie, recette; // Année de sortie et recette globale du film en millions de dollars
 	ListeActeurs acteurs;
 };
 
 struct Acteur
 {
-	std::string nom = ""; int anneeNaissance=0; char sexe=' ';
+	std::string nom ; int anneeNaissance; char sexe;
 	ListeFilms joueDans;
 };
